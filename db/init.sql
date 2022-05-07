b1e522382a51559607411e6ac7ad9518fc240354
Dmytro Liakhov <dmytro.liakhov@sezzle.com>
2022-05-07 12:31:16 +0300

    Create test data

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA query_analyzer;
 CREATE TABLE query_analyzer.database_query_info
 (
-    id                INT PRIMARY KEY NOT NULL,
+    id                SERIAL PRIMARY KEY NOT NULL,
     created_at        TIMESTAMP       NOT NULL,
     updated_at        TIMESTAMP,
     delete_at         TIMESTAMP,
@@ -11,3 +11,20 @@ CREATE TABLE query_analyzer.database_query_info
 
 CREATE INDEX idx_database_query_info_query ON query_analyzer.database_query_info (query);
 CREATE INDEX idx_database_query_info_delete ON query_analyzer.database_query_info (delete_at);
+
+-- insert test data
+INSERT INTO query_analyzer.database_query_info (created_at, updated_at, delete_at, query, execution_time_ms)
+SELECT (now() at time zone 'utc'), (now() at time zone 'utc'), null, 'SELECT * FROM users', round(random()*10000)
+FROM generate_series(1,20);
+
+INSERT INTO query_analyzer.database_query_info (created_at, updated_at, delete_at, query, execution_time_ms)
+SELECT (now() at time zone 'utc'), (now() at time zone 'utc'), null, 'UPDATE users SET age = ?', round(random()*10000)
+FROM generate_series(1,20);
+
+INSERT INTO query_analyzer.database_query_info (created_at, updated_at, delete_at, query, execution_time_ms)
+SELECT (now() at time zone 'utc'), (now() at time zone 'utc'), null, 'DELETE FROM users WHERE id = ?', round(random()*10000)
+FROM generate_series(1,20);
+
+INSERT INTO query_analyzer.database_query_info (created_at, updated_at, delete_at, query, execution_time_ms)
+SELECT (now() at time zone 'utc'), (now() at time zone 'utc'), null, 'INSERT INTO users (id, name, age) VALUES (?, ?, ?)', round(random()*10000)
+FROM generate_series(1,20);
